2956b7ad70366e8c38d97e9da931b7566454c9a6
yamamura <yamamurasouichiro@gmail.com>
2021-09-21 17:40:47 +0900

    update

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import {
-  StyleSheet, View, Text, TouchableOpacity, Alert,
+  StyleSheet, View, Text, TouchableOpacity, Alert, FlatList,
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import {
+  shape, string, instanceOf, arrayOf,
+} from 'prop-types';
 
-export default function MemoList() {
+export default function MemoList(props) {
+  const { memos } = props;
   const navigation = useNavigation();
-  return (
-    <View>
+
+  function renderItem({ item }) {
+    return (
       <TouchableOpacity
         style={styles.memoListItem}
         onPress={() => { navigation.navigate('MemoDetail'); }}
       >
         <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-          <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
+          <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
+          <Text style={styles.memoListItemDate}>{String(item.updatedAt)}</Text>
         </View>
         <TouchableOpacity
           style={styles.memoDelete}
@@ -24,11 +29,32 @@ export default function MemoList() {
           <Feather name="x" size={16} color="#B0B0B0" />
         </TouchableOpacity>
       </TouchableOpacity>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={memos}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
 
+MemoList.propTypes = {
+  memos: arrayOf(shape({
+    id: string,
+    bodyText: string,
+    updatedAt: instanceOf(Date),
+  })).isRequired,
+};
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   memoListItem: {
     backgroundColor: '#ffffff',
     flexDirection: 'row',
